1708763933a5476c78cdcc2ec5e9c8e8b02ef924
Geddy Delonge <57271262+RyanEllingson@users.noreply.github.com>
2020-09-26 23:09:14 -0500

    added database trial code

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -2,6 +2,10 @@ package servlet;
 
 import java.io.IOException;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.*;
+
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
@@ -17,9 +21,35 @@ public class HelloServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		ServletOutputStream out = res.getOutputStream();
-		out.write("hello heroku".getBytes());
-		out.flush();
-		out.close();
+		try {
+			ServletOutputStream out = res.getOutputStream();
+			Connection connection = getConnection();
+			
+			Statement stmt = connection.createStatement();
+			stmt.executeUpdate("DROP TABLE IF EXISTS ticks");
+			stmt.executeUpdate("CREATE TABLE ticks (tick timestamp)");
+			stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+			ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+			String result = "trying: ";
+			while (rs.next()) {
+				result += "Read from DB: " + rs.getTimestamp("tick");
+			}
+			out.write(result.getBytes());
+			out.flush();
+			out.close();
+		} catch (URISyntaxException | SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private static Connection getConnection() throws URISyntaxException, SQLException {
+		URI dbUri = new URI(System.getenv("DATABASE_URL"));
+		
+		String username = dbUri.getUserInfo().split(":")[0];
+		String password = dbUri.getUserInfo().split(":")[1];
+		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
+		
+		return DriverManager.getConnection(dbUrl, username, password);
 	}
 }
